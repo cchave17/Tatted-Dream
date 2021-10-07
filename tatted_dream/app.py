6440d9b9fc04866b0e92a3e2fed029f2ae387d75
cchave17 <carloschavezdeveloper@gmail.com>
2021-10-07 01:24:27 -0500

    added front end functionalitiy

--- tatted_dream/app.py
+++ tatted_dream/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 @app.route("/")
 def hello_world():
-    return render_template('./index.html')
+    return render_template('index.html')
 
 
 @app.route("/bookAppointment", methods=["POST"])
 def book_appointment():
-    pass
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+    return "FORM SUBMITTED"
